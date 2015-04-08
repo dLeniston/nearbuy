d65aa420b6954171e6200d0a781468f7e507e281
Darren Leniston <20054430@mail.wit.ie>
2015-04-08 13:46:26 +0100

    Adding Dynamic Store Info

--- app/models/store.rb
+++ app/models/store.rb
@@ -1,5 +1,9 @@
 class Store < ActiveRecord::Base
 	belongs_to :user 
+
+      geocoded_by :address
+      after_validation :geocode, :if => :address_changed?
+
       validates :user_id, presence: true
       validates :name, presence: true, length: { maximum: 140 }
       validates :address, presence: true, length: { maximum: 200}
@@ -7,6 +11,6 @@ class Store < ActiveRecord::Base
       					length: {in: 5..9}
       validates :vatno, presence: true, uniqueness: true, length: {is: 10}
       validates :opening, presence: true, length: {maximum: 200}
-      validates :latitude, presence: true
-      validates :longitude, presence: true
+      #validates :latitude, presence: true
+      #validates :longitude, presence: true
 end
